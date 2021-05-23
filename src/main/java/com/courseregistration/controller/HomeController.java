a28fbeafaed93082d9e249cb8c4f415286b7cad6
Laud Charles Ochei <>
2021-05-24 00:54:51 +0100

    Merge origin/main

---
+++ src/main/java/com/courseregistration/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.courseregistration.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {
+
+    
+
+    @GetMapping(value={"/", "/home"})
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+
+
+
+
+}
